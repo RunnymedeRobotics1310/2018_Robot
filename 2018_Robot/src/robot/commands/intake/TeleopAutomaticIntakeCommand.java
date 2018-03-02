884bafa50bc49f04ace1e4aad306cde42ce633e3
colen <colen@10.13.236.27>
2018-03-01 21:19:41 -0500

    added teleop automatic intake command

--- 2018_Robot/src/robot/commands/intake/TeleopAutomaticIntakeCommand.java
+++ 2018_Robot/src/robot/commands/intake/TeleopAutomaticIntakeCommand.java
@@ -5,6 +5,10 @@ import robot.Robot;
 
 public class TeleopAutomaticIntakeCommand extends Command {	
 	
+	public TeleopAutomaticIntakeCommand(){
+		requires(Robot.intakeSubsystem);
+	}
+	
 	protected void initialize() {
 		Robot.intakeSubsystem.intakeCube();
 		Robot.intakeSubsystem.intakeClawOpen();
@@ -14,6 +18,9 @@ public class TeleopAutomaticIntakeCommand extends Command {
 		if (Robot.intakeSubsystem.isCubeDetected()) {
 			Robot.intakeSubsystem.intakeStop();
 			Robot.intakeSubsystem.intakeClawClose();
+			return true;
+		}
+		if (Robot.oi.getCancelCommand()) {
 			return true;
 		}
 		return false;
